f07799ed0e4e219b11fa35a2b4ecdd398664faad
diehaa <dieumy430@gmail.com>
2025-08-26 01:19:40 +0700

    test

--- TestAPI/Controllers/ConfigTestController.cs
+++ TestAPI/Controllers/ConfigTestController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient; // namespace mới
 
 namespace TestAPI.Controllers
 {
@@ -53,5 +54,34 @@ namespace TestAPI.Controllers
                 ConnectionString = connString
             });
         }
+        [HttpGet("test-users")]
+        public IActionResult TestUsers()
+        {
+            var connString = _configuration.GetConnectionString("DefaultConnection");
+            var users = new List<object>();
+
+            using (var conn = new SqlConnection(connString))
+            {
+                conn.Open();
+                using (var cmd = new SqlCommand("SELECT TOP 10 Id, UserName, Email FROM AppUsers", conn))
+                using (var reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        users.Add(new
+                        {
+                            Id = reader["Id"],
+                            UserName = reader["UserName"],
+                            Email = reader["Email"]
+                        });
+                    }
+                }
+            }
+
+            return Ok(users);
+        }
+
     }
+
+
 }
